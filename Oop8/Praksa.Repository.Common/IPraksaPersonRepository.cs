a61867b550f95c7b8ef1ed14d9972bc6afdfef88
Denis <denis_poljak@hotmail.com>
2020-06-10 12:13:08 +0200

    DI and container

---
+++ Oop8/Praksa.Repository.Common/IPraksaPersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using Praksa.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praksa.Repository.Common
+{
+    public interface IPraksaPersonRepository
+    {
+        Task <List<Person>> GetAllPeopleAsync();
+        Task UpdatePersonAsync(Person person);
+        Task DeletePersonAsync(Person person);
+        Task AddPersonAsync(Person person);
+    }
+}
---
+++ Oop8/Praksa.Service.Common/IPraksaPersonService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Praksa.Model;
+namespace Praksa.Service.Common
+{
+    public interface IPraksaPersonService
+    {
+        Task<List<Person>> GetAllPeopleAsync();
+        Task UpdatePersonAsync(Person person);
+        Task DeletePersonAsync(Person person);
+        Task AddPersonAsync(Person person);
+    }
+}
